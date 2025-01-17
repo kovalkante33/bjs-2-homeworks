53afcb1e01dd438ecf91161b8073ad1fe5d075d2
Алексей Балуев <kovalkante33@gmail.com>
2025-01-17 10:10:25 +0300

    Второе задание промежуточная готовность_2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -18,20 +18,67 @@ function getArrayParams(...arr) {
   return { min: min, max: max, avg: avg };
 }
 
+// Функция для нахождения суммы элементов массива
 function summElementsWorker(...arr) {
-
+  if (!arr || arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((acc, el) => acc + el, 0);
 }
 
+// Функция для вычисления разницы максимального и минимального элементов
 function differenceMaxMinWorker(...arr) {
+  if (!arr || arr.length === 0) {
+    return 0;
+  }
 
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+
+  return max - min;
 }
 
+// Функция для вычисления разницы сумм чётных и нечётных элементов
 function differenceEvenOddWorker(...arr) {
+  if (!arr || arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let el of arr) {
+    if (el % 2 === 0) {
+      sumEvenElement += el;
+    } else {
+      sumOddElement += el;
+    }
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
+// Функция для вычисления среднего значения чётных элементов
 function averageEvenElementsWorker(...arr) {
+  if (!arr || arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let el of arr) {
+    if (el % 2 === 0) {
+      sumEvenElement += el;
+      countEvenElement++;
+    }
+  }
+
+  if (countEvenElement === 0) {
+    return 0;
+  }
+
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork(arrOfArr, func) {
