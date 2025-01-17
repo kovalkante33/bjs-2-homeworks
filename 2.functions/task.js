611b8725be047f1cc74fd5476d6f6f30f7d6e29b
Алексей Балуев <kovalkante33@gmail.com>
2025-01-17 09:49:44 +0300

    Второе задание промежуточная готовность

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,19 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  let avg = (sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
@@ -19,6 +34,6 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
 
 }
