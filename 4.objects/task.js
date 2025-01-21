cc2a1cbd8e5b20dfec25dda4ae787a12c39993dd
Алексей Балуев <kovalkante33@gmail.com>
2025-01-21 09:03:17 +0300

    Четвёртое задание 2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,7 +9,7 @@ Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
+Student.prototype.addMarks = function (...marksToAdd) {
     if (this.marks === undefined) {
         console.log()
     } else {
